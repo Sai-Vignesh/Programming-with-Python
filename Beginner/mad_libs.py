2eed66a094d83b42285b7edfe00c62ee7c1ab027
Sai Vignesh <saivigneshnaragoni@gmail.com>
2022-10-20 14:00:55 +0530

    Create mad_libs.py

---
+++ Beginner/mad_libs.py
@@ -0,0 +1,8 @@
+# Mad Lib Program
+
+name = input("Enter a name: ")
+age = input("Enter age: ")
+profession: input("Enter a profession: ")
+color = input("Enter a color: ")
+
+print(f"Hi, I'm {name}. I am {age} years old. I am a {profession}. My favorite color is {color}.")
