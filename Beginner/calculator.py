24ae2bc43f0fb35daae0165c6afce78c4279e3b4
Sai Vignesh <saivigneshnaragoni@gmail.com>
2022-10-20 14:22:26 +0530

    Create calculator.py

---
+++ Beginner/calculator.py
@@ -0,0 +1,54 @@
+# Program make a simple calculator
+
+# This function adds two numbers
+def addition(x, y):
+    return x + y
+
+# This function subtracts two numbers
+def subtraction(x, y):
+    return x - y
+
+# This function multiplies two numbers
+def multiplication(x, y):
+    return x * y
+
+# This function divides two numbers
+def division(x, y):
+    return x / y
+
+
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+while True:
+    # take input from the user
+    choice = input("Enter choice(1/2/3/4): ")
+
+    # check if choice is one of the four options
+    if choice in ('1', '2', '3', '4'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '1':
+            print(num1, "+", num2, "=", addition(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtraction(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiplication(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", division(num1, num2))
+        
+        # check if user wants another calculation
+        # break the while loop if answer is no
+        next_calculation = input("Let's do next calculation? (yes/no): ")
+        if next_calculation == "no" or next_calculation == "n":
+          break
+    
+    else:
+        print("Invalid Input")
