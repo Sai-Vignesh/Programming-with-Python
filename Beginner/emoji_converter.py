2a7d339f85ea57bf45b2fe4d11e50e6b155624b0
Sai Vignesh <saivigneshnaragoni@gmail.com>
2022-10-20 14:37:21 +0530

    Create emoji_converter.py

---
+++ Beginner/emoji_converter.py
@@ -0,0 +1,18 @@
+# Emoji converter program
+
+message = input("> ")
+words = message.split(" ")
+
+emojis = {
+    ":)" : 😊,
+    ":(" : 😞, 
+}
+
+output = ""
+for word in words:
+    output += emojis.get(word, word) + " "
+    
+print(output)
+
+## > good morning sunshine :)
+# result => good morning sunshine 😊
